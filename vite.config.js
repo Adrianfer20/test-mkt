733f1bf2b32a5d40a8808b7ff121f6054582f2a8
Adrianfer <35809235+Adrianfer20@users.noreply.github.com>
2024-06-26 16:21:59 -0400

    gh-pages add 4

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://adrianfer20.github.iu/test-mkt'
+  base: 'https://adrianfer20.github.io/test-mkt/'
 })
